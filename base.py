b6cb037f628dbe7ae58dcca41755875b2412e52a
gloriaodipo <gloriaodipo@gmail.com>
2018-06-25 16:27:15 +0300

    [CHORE] add tests

---
+++ base.py
@@ -0,0 +1,28 @@
+from app.views import app
+import unittest
+import json
+
+class BaseClass(unittest.TestCase):
+    """This is the base class for test cases."""
+
+    def setUp(self):
+        """Initialize app and define test variables"""
+        self.app = app
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.user_data = {
+                    "username":"carenakinyi", 
+                    "department":"finance",
+                    "email":"caren@gmail.com",
+                    "password":"passw"
+                    }
+        self.request_data = {
+                    "req_id": 4,
+                    "request_title": "Faulty chair",
+                    "description": "blablablaaaaaaaaaa"
+                    }
+
+    def tearDown(self):
+        """ Clear anything that has been saved. """
+        pass                    
